1f62cfd0ef60eb6007169c7acc5624a68bce2b87
buttonwild <975505965@qq.com>
2021-02-23 16:54:02 +0800

    20210223通过两维数组查找

--- project01/Double_Arr.go
+++ project01/Double_Arr.go
@@ -1,31 +1,38 @@
 package main
 
-import "fmt"
-
 func main() {
 	matrix := [][]int{
-		{1, 4, 7, 11, 15},
-		{2, 5, 8, 12, 19},
-		{3, 6, 9, 16, 22},
-		{10, 13, 14, 17, 24},
-		{18, 21, 23, 26, 30},
-	}
-	target := 5
+		{-5}}
+	target := 0
 	findNumberIn2DArray(matrix, target)
 
 }
 
 func findNumberIn2DArray(matrix [][]int, target int) bool {
-    row := 0
-    cloumn := len(matrix[0])-1
+	//考虑三种异常
+	//第一空数组
+	//第二只存在一个数
+	//第三不存在
+	row := 0
+	cloumn := 0
+	if len(matrix) == 0 {
+		return false
+	} else {
+		cloumn = len(matrix[0]) - 1
+	}
 
-	n := matrix[row][cloumn]
-	if target==n{
-        return true
-    }
-    else target<n {
-        cloumn--
-        n := matrix[row][cloumn]
-    }
-	return true
+	for (row < len(matrix)) && (cloumn > -1) {
+		n := matrix[row][cloumn]
+		if target == n {
+			return true
+		}
+		if target < n {
+			cloumn--
+		}
+		if target > n {
+			row++
+		}
+
+	}
+	return false
 }
