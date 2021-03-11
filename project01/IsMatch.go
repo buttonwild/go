c1d2dd9d97b9193615e78bea5ad70ba0fbdba16f
buttonwild <975505965@qq.com>
2021-03-12 05:50:29 +0800

    20210312更新正则表达式匹配，思路为前面匹配完成后，剩余部分检索看是否有匹配内容，递归

--- project01/IsMatch.go
+++ project01/IsMatch.go
@@ -3,46 +3,100 @@ package main
 import "fmt"
 
 func main() {
-	s := "aa"
-	p := "a*"
+	s := "aaa"
+	p := "ab*a*c*a"
 	fmt.Println(isMatch(s, p))
 }
 func isMatch(s string, p string) bool {
+	var f []int
 	var b byte = 65
 	var t byte = 32
+	s2 := s
 	for len(s) != 0 {
+		if len(p) == 0 {
+			return false
+		}
 		i := 0
 		if s[0] == p[i] {
 			s = s[1:]
 			b = p[i]
 			i++
-			if i == len(p) {
-				return false
+			if i != len(p) {
+				p = p[i:]
+				if p[0] != '*' {
+					b = t
+				}
+			} else {
+				p = p[i:]
 			}
-			p = p[i:]
-			if p[0] != '*' {
-				b = t
-			}
+
 		} else if s[0] != p[i] && p[i] == '.' {
 			s = s[1:]
 			b = p[i]
 			i++
-			if i == len(p) {
-				return false
+			if i != len(p) {
+				p = p[i:]
+				if p[0] != '*' {
+					b = t
+				}
+			} else {
+				p = p[i:]
 			}
-			p = p[i:]
 		} else if s[0] != p[i] && i < len(p)-1 && p[i+1] == '*' {
 			p = p[2:]
 		} else if p[i] == '*' {
 			if b != s[0] && b != '.' {
-				return false
+				p = p[1:]
+			} else if b == s[0] || b == '.' {
+				s = s[1:]
+			} else if i != len(p)-1 && p[1] == b {
+				p = string(p[0]) + p[i:]
 			}
-			s = s[1:]
-			p = p[i+1:]
+		} else {
+			return false
 		}
 	}
-	if len(p) != 0 {
-		return false
+	for len(p) != 0 {
+		if p[0] != '*' {
+			if len(p) >= 2 {
+				if p[1] == '*' {
+					p = p[2:]
+				} else {
+					return false
+				}
+			} else {
+				return false
+			}
+		} else {
+			p = p[1:]
+			if len(p) != 0 {
+				for z := 0; z < len(s2); z++ {
+					if s2[z] == p[0] {
+						f = append(f, z)
+					}
+				}
+				for len(f) != 0 {
+					if isMatch(s2[f[0]:], p) == true {
+						return true
+					} else {
+						if len(p) >= 1 {
+							f = f[1:]
+						} else {
+							return false
+						}
+					}
+				}
+				if len(p) >= 2 {
+					if p[1] == '*' {
+						p = p[2:]
+					} else {
+						return false
+					}
+				} else {
+					return false
+				}
+			}
+		}
 	}
 	return true
 }
